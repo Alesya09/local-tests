680b2f4c300e64d5c4812e91a4ad1f33f45f06ce
Alesya09 <annalesh@mail.ru>
2022-11-12 18:54:42 -0600

    page-elements

--- cypress/e2e/profile.cy.js
+++ cypress/e2e/profile.cy.js
@@ -1,41 +1,62 @@
+import ProfilePage from "../pages/app/profile.page";
+
 describe('Profile', () => {
 
   beforeEach(() => {
 
     cy.login(Cypress.env('TOKEN'), Cypress.env('USER_ID'))
+    cy.intercept(
+      'GET,' +
+      '/course/coursesProgress/*',
+      {
+        statusCode: 200,
+        body: {
+
+          "message": "Get courses progress",
+          "success": true,
+          "fail": false,
+          "payload": [
+            {
+              "_id": "628429d4f2a616614cb68bdb",
+              "completedLessons": 111,
+              "totalLessons": 88,
+              "course": {
+                "_id": "5ff2005cacc2d5003ae26bc7",
+                "name": "QA Manual"
+              }
+            }
+          ]
+        }
+      }
+      )
 
     cy.visit(`/profile/${Cypress.env('USER_ID')}`)
-
   })
 
-  it('Daily report creation', () => {
+  it.skip('Daily report creation', () => {
     const timestamp = new Date().getTime()
     const description = `${timestamp} 123456789012345678901234567890`
 
     cy.get('[data-qa="dailyReportsBtn"]')
       .click()
-
-
     cy.get('input[value="help_classmates"]')
        .click()
-
     cy.get('[id="labels.help_classmates.hours"]')
       .type('1')
-
     cy.get('#morale')
       .click()
-
     cy.get('.ant-select-item[title="5"]')
       .click()
-
     cy.get('textarea.ant-input')
       .type(description)
-
     cy.get('[type="submit"]')
       .click()
-
     cy.xpath(`//div[@class="ant-row mb-4"][contains(text(), "${timestamp}")]`)
       .should('be.visible')
-
   })
+
+  it('Courses in progress', () =>{
+    ProfilePage.headerCoursesProgress.should('be.visible')
+    ProfilePage.courseProgress.should('have.text', '31%2%27%0%5%4%')
+    })
 })
